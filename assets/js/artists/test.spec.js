7196920055c1ef05056394143c23b092bc216fe8
wesssel <wessel.vanderpal@vergleich.org>
2018-01-21 15:25:47 +0900

    add tests for home

--- assets/js/artists/test.spec.js
+++ assets/js/artists/test.spec.js
@@ -2,11 +2,53 @@ import { expect } from 'chai'
 import { mount } from '@vue/test-utils'
 import Artists from './index.vue'
 
+const mock = {
+  artists: [
+    {
+      title: 'Artist 1 name',
+      origin: 'Country 1',
+      imageUrl: 'http://via.placeholder.com/350x150',
+    },
+    {
+      title: 'Artist 2 name',
+      origin: 'Country 2',
+      imageUrl: 'http://via.placeholder.com/350x120',
+    },
+  ],
+}
+
 describe('Artists', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(Artists, {
+      data: {
+        artists: mock.artists,
+      },
+    })
+  })
+
   it('h1 to equal artists', () => {
-    const wrapper = mount(Artists)
     const $h1 = wrapper.find('h1')
 
     expect($h1.text()).to.equal('Artists')
+  })
+
+  it('artist title to be in view', () => {
+    mock.artists.forEach((artist) => {
+      expect(wrapper.text()).to.contain(artist.title)
+    })
+  })
+
+  it('artist origin to be in view', () => {
+    mock.artists.forEach((artist) => {
+      expect(wrapper.text()).to.contain(artist.origin)
+    })
+  })
+
+  it('artist image to be in view', () => {
+    mock.artists.forEach((artist) => {
+      expect(wrapper.html()).to.contain(artist.imageUrl)
+    })
   })
 })
